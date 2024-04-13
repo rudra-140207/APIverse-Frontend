0a7b2c02863dee065d2b1df910d73c94adb93128
RUDRA PRATAP SINGH <rudra.2226en1029@kiet.edu>
2024-04-13 15:19:54 +0530

    added media query

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -7,7 +7,6 @@ import axios from "axios";
 
 const Demo = () => {
   const [generatedKey, setGeneratedKey] = useState("Temp key");
-
   const [userCode, setUserCode] = useState(
     `axios.get("http://localhost:4000/api/movie/query?year=2001&cast=aamir",{
       headers : {
@@ -15,7 +14,6 @@ const Demo = () => {
       }
     })`
   );
-
   const [output, setOutput] = useState("Output will be displayed here !!");
 
   const handleGenerateKey = () => {
@@ -29,6 +27,16 @@ const Demo = () => {
       });
   };
 
+  const handleReset = () => {
+    setUserCode(
+      `axios.get("http://localhost:4000/api/movie/query?year=2001&cast=aamir",{
+      headers : {
+        auth_key : "place-auth-key-here"
+      }
+    })`
+    );
+  };
+
   const handleGenerateOutput = () => {
     eval(userCode)
       .then((res) => {
@@ -38,7 +46,7 @@ const Demo = () => {
         console.log(err);
         if (err.response) {
           setOutput(
-    `Request failed with status ${err.response.status} :${err.response.data.message}`
+            `Request failed with status ${err.response.status}: ${err.response.data.message}`
           );
         } else if (err.request) {
           setOutput("Request failed: No response received, Check response URL");
@@ -51,7 +59,7 @@ const Demo = () => {
   return (
     <div className="mt-8 flex flex-col items-center justify-center min-h-screen">
       <Heading title1="Test our API" title2="here" />
-      <p className="text-lightText mt-5 text-start">
+      <p className="text-lightText m-5 text-start">
         Just generate a temporary key from here and use it to test the API.
       </p>
       <p className="text-red-400 mt-1 text-start">
@@ -62,22 +70,43 @@ const Demo = () => {
         <Button title="Generate new Key" />
       </Link>
 
-      <div className="w-full max-w-md bg-white rounded-lg shadow-md p-4 mt-8 mb-8">
+      <div className="w-full max-w-md bg-white rounded-lg shadow-md p-4 mt-8 mb-8 lg: w-2/3">
         <p className="text-gray-800">{generatedKey}</p>
       </div>
 
-      <textarea
-        className="w-full max-w-md h-48 bg-white rounded-lg shadow-md p-4 mb-8"
-        value={userCode}
-        onChange={(e) => setUserCode(e.target.value)}
-        placeholder="Write your code here..."
-      />
-      <Link onClick={handleGenerateOutput}>
-        <Button title="Generate Output" />
-      </Link>
+      <div className="flex flex-col lg:flex-row justify-between w-full max-w-7xl mx-auto px-4 lg: w-11/12">
+        <div className="w-full lg:w-1/2 lg:mr-16">
+          <p className="text-lightText mt-5 text-start">
+            Change the query accordingly, refer to{" "}
+            <Link className="text-blue-500" to="/docs">
+              Documentation
+            </Link>{" "}
+            .
+          </p>
+          <textarea
+            className="w-full h-48 bg-white rounded-lg text-lg shadow-md p-4 mt-4 mb-8 lg: text-sm lg: mb-4"
+            value={userCode}
+            onChange={(e) => setUserCode(e.target.value)}
+          />
+          <div className="flex">
+            <div className="w-1/4">
+              <Link onClick={handleReset}>
+                <Button title="Reset" />
+              </Link>
+            </div>
 
-      <div className="w-full max-w-lg h-96 mt-8 mb-16 bg-white rounded-lg shadow-md p-4 overflow-y-auto">
-        <pre className="text-gray-800">{output}</pre>
+            <div className="w-2/3 ml-8">
+              <Link onClick={handleGenerateOutput}>
+                <Button title="Generate Output" />
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="w-full lg:w-1/2 mt-8 lg:mt-0 lg: mb-4">
+          <div className="h-96 bg-white rounded-lg shadow-md p-4 overflow-y-auto">
+            <pre className="text-gray-800">{output}</pre>
+          </div>
+        </div>
       </div>
     </div>
   );
