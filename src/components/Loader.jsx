1840cdabd5a3cc65dc5314083e1862cbfca141f2
RUDRA PRATAP SINGH <rudra.2226en1029@kiet.edu>
2024-05-05 23:37:33 +0530

    added loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,9 +3,11 @@ import React from 'react';
 const Loader = () => {
   return (
     <div className="flex justify-center items-center">
-      <div className="loader ease-linear rounded-full border-4 border-t-4 border-brightGreen h-6 w-6"></div>
+      <div className="loader ease-linear rounded-full border-4 border-t-4 border-green-500 h-6 w-6 animate-spin"></div>
     </div>
+    
   );
 };
 
 export default Loader;
+
