7afc4ca1e2e5e5a8b984d220553fd2338546d9c1
RUDRA PRATAP SINGH <rudra.2226en1029@kiet.edu>
2024-05-05 23:03:38 +0530

    loading

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Home from "./Home";
 import About from "./About";
 import Courses from "./Courses";
 import Reviews from "./Reviews";
+import axios from "axios";
+import reqUrl from "../service";
 
 const Landing = () => {
+  useEffect(() => {
+    axios
+    .get(`${reqUrl}/wakeup`)
+    .then((res)=>{
+      console.log("Initiated");
+    }).catch((err)=>{
+      console.log(err);
+    })
+  }, [])
+  
   return (
     <div>
       <Home />
